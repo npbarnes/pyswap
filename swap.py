c44bc6a5294af14acde34162e78a9e975e4cb096
Nathan Barnes <npbarnes@alaska.edu>
2017-11-14 17:51:33 -0900

    Update plotting

--- swap.py
+++ swap.py
@@ -321,30 +321,56 @@ def trajectory(t1, t2, dt):
         o[i,:]   = NH_tools.orientation_at_time(t)
 
     return pos, o
+
         
 
 if __name__ == '__main__':
+    from matplotlib import rcParams
     import matplotlib.pyplot as plt
     from matplotlib.colors import LogNorm
-    para, xp, v, mrat, beta, tags = open_stuff('/home/nathan/data/pluto-2/databig')
-    v = v.astype(np.float64)
+    rcParams['figure.figsize'] = (rcParams['figure.figsize'][0], 2*rcParams['figure.figsize'][1])
 
-    #points, o = trajectory(NH_tools.close_start, NH_tools.close_end, 600.)
 
-    points = np.zeros((50,3),dtype='d')
-    points[:,0] = np.linspace(20,-100,50)*1187.
-    points[:,2] = np.max(para['qz'])/2
+    print("Reading particle data...")
+    para, xp, v, mrat, beta, tags = particle_data('/home/nathan/data/pluto-2/databig', n=2)
 
-    o = np.zeros((50,3),dtype='d')
-    o[:,2] = 90.
+    print("Loading trajectory information...")
+    points, o = trajectory(NH_tools.close_start, NH_tools.close_end, 60.)
+    
+    xp = xp[(tags != 0)]# & (mrat == 1./16.)]
+    v = v[(tags != 0)]# & (mrat == 1./16.)]
+    beta = beta[(tags != 0)]# & (mrat == 1./16.)]
+    mrat = mrat[(tags != 0)]# & (mrat == 1./16.)]
 
-    xp = xp[tags != 0]
-    v = v[tags != 0]
-    mrat = mrat[tags != 0]
-    beta = beta[tags != 0]
+    print("Generating spectrograms...")
+    H, He, CH4 = spectrograms_by_species(xp, v, mrat, beta, points, o, radius=1187., progress=True)
 
-    sp = spectrogram(xp, v, mrat, beta, points, o)
-    plt.pcolormesh(sp.T, norm=LogNorm())
-    plt.gca().set_yscale('log')
-    plt.colorbar()
+    print("Plotting...")
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+
+    # The actual spectrogram to be plotted will be the total response of all species
+    tot_spec = H + He + CH4
+
+    # but it will be colored by which species contributes the most counts to that bin
+    # so we need masks for where each species dominates.
+    mH = np.ma.masked_array(tot_spec, mask=(~((H>He) & (H>CH4))))
+    mHe = np.ma.masked_array(tot_spec, mask=(~((He>H) & (He>CH4))))
+    mCH4 = np.ma.masked_array(tot_spec, mask=(~((CH4>H) & (CH4>He))))
+
+    # Plot the masked arrays
+    Hhist = ax1.pcolormesh(points[:,0]/1187., bin_centers, mH.T, norm=LogNorm(), cmap='Blues',
+            vmin=2e-3, vmax=2e4)
+
+    Hehist = ax1.pcolormesh(points[:,0]/1187., bin_centers, mHe.T, norm=LogNorm(), cmap='Greens',
+            vmin=2e-3, vmax=2e4)
+
+    CH4hist = ax1.pcolormesh(points[:,0]/1187., bin_centers, mCH4.T, norm=LogNorm(), cmap='Reds',
+            vmin=2e-3, vmax=2e4)
+
+    ax1.invert_xaxis()
+    ax1.set_yscale('log')
+
+    ax2.plot(points[:,0]/1187., o)
+
     plt.show()
+
