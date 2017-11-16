846c1b574ce11362c4ec56f7ce5273d4695c7de3
Nathan Barnes <npbarnes@alaska.edu>
2017-11-16 13:50:41 -0900

    Updated version of grid_profiles to work with all my latest tools

--- grid_profiles.py
+++ grid_profiles.py
@@ -1,91 +1,73 @@
 #!/usr/bin/python
-from sys import argv
 import numpy as np
-from scipy.io import readsav
 from scipy.interpolate import RegularGridInterpolator
+from scipy.constants import value, unit
 from HybridReader2 import HybridReader2 as hr
-from pprint import pprint
+from HybridParams import HybridParams
 import matplotlib.pyplot as plt
+import NH_tools
+import argparse
 
-k = 1.38064852e-23 # Boltzmann constant in SI units [m^2 kg s^-2 K^-1] == [J/K]
+eV2J = value('Boltzmann constant in eV/K')
+k = value('Boltzmann constant')
+assert unit('Boltzmann constant') == 'J K^-1'
 
-p = hr(argv[1],'up')
-traj = readsav('traj.sav')['traj']
-traj = np.dstack((traj.x[0],traj.y[0]))[0]
+parser = argparse.ArgumentParser()
+parser.add_argument('hybrid_folder')
+args = parser.parse_args()
 
-qx = p.para['qx']
-qy = p.para['qy']
-cz = p.para['zrange']/2
-
-try:
-    po = p.para['pluto_offset']
-except KeyError:
-    po = 30
-
-Rp = 1187
-
-traj[:,0] = (traj[:,0] - qx[len(qx)/2 + po])/Rp
-traj[:,1] = (traj[:,1] - qy[len(qy)/2])/Rp
-qx = (qx - qx[len(qx)/2 + po])/Rp
-qy = (qy - qy[len(qy)/2])/Rp
-
-def vprofile():
-    v = hr(argv[1],'up')
-    vdata = v.get_last_timestep()[-1]
-    vmag = np.sqrt(vdata[:,:,cz,0]**2 + vdata[:,:,cz,1]**2 + vdata[:,:,cz,2]**2)
-
-    rgi = RegularGridInterpolator(points=[qx,qy], values=vmag, bounds_error=False)
-    vinterp = rgi(traj)
-
-    return vinterp
-
-def nprofile():
-    n = hr(argv[1],'np')
-    ndata = n.get_last_timestep()[-1]
-    # Convert to cm^-3
-    ndata = ndata*1e-15
-
-    rgi = RegularGridInterpolator(points=[qx,qy], values=ndata[:,:,cz], bounds_error=False)
-    ninterp = rgi(traj)
-
-    return ninterp
-
-def tprofile():
-    t = hr(argv[1],'temp')
-    tdata = t.get_last_timestep()[-1]
-    # Convert from eV to K
-    tdata = tdata/(k*6.242e18)
-
-    rgi = RegularGridInterpolator(points=[qx,qy], values=tdata[:,:,cz], bounds_error=False)
-    tinterp = rgi(traj)
-
-    return tinterp
+para = HybridParams(args.hybrid_folder).para
 
 
-v = vprofile()
-n = nprofile()
-t = tprofile()
-p = k*n*t*(100**3)*(1e12) #pressure in pPa
+def pluto_position(p):
+    """get the position of pluto in simulation coordinates"""
+    return p['qx'][p['nx']/2 + p['pluto_offset']]
 
-f, (av,an,at,ap) = plt.subplots(4, sharex=True)
-# Settings for the shared x axis
-av.invert_xaxis()
-av.set_xlim([-10,-100])
-ap.set_xlabel('X [R$_p$]')
+qx = para['qx'] - pluto_position(para)
+qy = para['qy'] - para['qy'][-1]/2
+qz = para['qzrange'] - para['qzrange'][-1]/2
 
-av.plot(qx[::2],v)
-an.plot(qx[::2],n)
-at.plot(qx[::2],t)
-ap.plot(qx[::2],p)
+grid_points = qx, qy, qz
 
-av.set_ylabel('<v$_{flow}$> [km/s]')
-an.set_ylabel('Density [cm$^{-3}$]')
-at.set_ylabel('Temp [K]')
-ap.set_ylabel('nkT [pPa]')
+def profile(interp_points, grid_points, grid_data):
+    rgi = RegularGridInterpolator(points=grid_points, values=grid_data, bounds_error=False)
+    return rgi(interp_points)
 
-an.set_yscale('log')
-at.set_yscale('log')
-ap.set_yscale('log')
+points, o = NH_tools.trajectory(NH_tools.close_start, NH_tools.close_end, 60.)
 
-f.savefig('profiles.png', format='png', bbox_inches='tight')
-f.clear()
+v = hr(args.hybrid_folder,'up').get_last_timestep()[-1]
+vdata = np.linalg.norm(v, axis=-1)
+v_profile = profile(points, grid_points, vdata)
+
+n = hr(args.hybrid_folder, 'np').get_last_timestep()[-1]
+ndata = n*1e-15
+n_profile = profile(points, grid_points, ndata)
+
+t = hr(args.hybrid_folder,'temp_p').get_last_timestep()[-1]
+tdata = t/eV2J
+t_profile = profile(points, grid_points, tdata)
+
+pdata = k*ndata*tdata*(100**3)*(1e12) #pressure in pPa
+p_profile = profile(points, grid_points, pdata)
+
+fig, (vax, nax, tax, pax) = plt.subplots(4, sharex=True)
+vax.invert_xaxis()
+vax.set_xlim([-10,-100])
+pax.set_xlabel('X [$R_p$]')
+
+vax.plot(points[:,0]/1187., v_profile)
+nax.plot(points[:,0]/1187., n_profile)
+tax.plot(points[:,0]/1187., t_profile)
+pax.plot(points[:,0]/1187., p_profile)
+
+vax.set_ylabel('<v$_{flow}$> [km/s]')
+nax.set_ylabel('Density [cm$^{-3}$]')
+tax.set_ylabel('Temp [K]')
+pax.set_ylabel('nkT [pPa]')
+
+nax.set_yscale('log')
+tax.set_yscale('log')
+pax.set_yscale('log')
+
+plt.show()
+
