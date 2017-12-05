318b55d49e227f02bf120e0dce80638913b844d8
Nathan Barnes <npbarnes@alaska.edu>
2017-12-05 10:13:34 -0900

    Trajectory context plot and some extra testing.

--- espec.py
+++ espec.py
@@ -58,9 +58,24 @@ def plot_espec(fig, ax, cbar_H, cbar_He, cbar_CH4, espec):
 
     # add a title
     Hecb.ax.set_title('SCEM (Hz)', fontdict={'fontsize':'small'})
-
     ax.invert_xaxis()
+
     ax.set_yscale('log')
+
+def plot_traj_context(fig, ax_xy, ax_xz, prefix, traj, size, mccomas=False):
+    hybrid_np_CH4 = hr(prefix, 'up')
+    np_3d = hybrid_np_CH4.get_timestep(-1)[-1][:,:,:,0]
+    hh.direct_plot(fig, ax_xy, np_3d, hybrid_np_CH4.para, 'xy', norm=None, fontsize=size, mccomas=mccomas)
+    hh.direct_plot(fig, ax_xz, np_3d, hybrid_np_CH4.para, 'xz', norm=None, fontsize=size, mccomas=mccomas)
+
+    traj = traj/1187.
+
+    if mccomas:
+        traj[:,0] = -traj[:,0]
+        traj[:,1] = -traj[:,1]
+    ax_xy.plot(traj[:,0], traj[:,1], color='black', linewidth=2, scalex=False, scaley=False)
+    ax_xz.plot(traj[:,0], traj[:,2], color='black', linewidth=2, scalex=False, scaley=False)
+
 
 def three_colorbars(fig, ax):
     fig.subplots_adjust(right=0.8, hspace=0.05)
@@ -77,8 +92,46 @@ def three_colorbars(fig, ax):
 
     return cbar_H, cbar_He, cbar_CH4
 
-
 if __name__ == '__main__':
+    fontsize = 20
+
+    fig, (ax1,ax2) = plt.subplots(ncols=2, sharex=True, figsize=(2.2*rcParams['figure.figsize'][0], rcParams['figure.figsize'][1]))
+
+    ax1.set_aspect('equal')
+    ax2.set_aspect('equal')
+
+    points, o = NH_tools.trajectory(NH_tools.flyby_start, NH_tools.flyby_end, 60.)
+    plot_traj_context(fig, ax1, ax2, "/home/nathan/data/2017-Mon-Nov-13/pluto-8/data", points, fontsize, mccomas=True)
+    ax1.set_xlim([-45, ax1.get_xlim()[1]])
+
+    fig.suptitle("Heavy Ion Density", fontsize=1.5*fontsize)
+
+    plt.tight_layout(rect=[0,0.03,1,0.9])
+    plt.show()
+
+if __name__ == '__pain__':
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('prefix', help='Path to the hybrid data folder')
+    parser.add_argument('-n', type=int, default=2, help='Number of z direction slices to use')
+
+    args = parser.parse_args()
+
+    fig, (ax, ax2) = plt.subplots(2)
+    cbar_H, cbar_He, cbar_CH4 = three_colorbars(fig, ax)
+    ax.set_title('Synthetic SWAP Spectrogram\n0.3nT IMF')
+
+    plot_espec(fig, ax, cbar_H, cbar_He, cbar_CH4, args.prefix, args.n)
+    points, o = NH_tools.trajectory(NH_tools.close_start, NH_tools.close_end, 60.)
+
+    ax2.plot(points[:,0], o)
+
+
+    plt.show()
+
+
+if __name__ == '__pain__':
     import argparse
     parser = argparse.ArgumentParser()
 
@@ -89,8 +142,10 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots()
     cbar_H, cbar_He, cbar_CH4 = three_colorbars(fig, ax)
+    ax.set_title('Synthetic SWAP Spectrogram\n0.3nT IMF')
 
     plot_espec(fig, ax, cbar_H, cbar_He, cbar_CH4, args.prefix, args.n)
+
 
     plt.show()
 
