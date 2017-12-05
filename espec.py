7e06ef4b6b985145d238a029a2bedc7574329a36
Nathan Barnes <npbarnes@alaska.edu>
2017-12-05 10:11:54 -0900

    Separate producing and plotting the spectrogram.

--- espec.py
+++ espec.py
@@ -2,26 +2,28 @@
 import numpy as np
 import NH_tools
 from swap import spectrograms_by_species, bin_centers, is_sunview
+from HybridReader2 import HybridReader2 as hr
+import HybridHelper as hh
 from HybridParticleReader import particle_data
 from matplotlib import rcParams
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
-def plot_espec(fig, ax, cbar_H, cbar_He, cbar_CH4, prefix, n):
+def get_espec(prefix, n, traj=None):
+    if traj is None:
+        points, o = NH_tools.trajectory(NH_tools.flyby_start, NH_tools.flyby_end, 60.)
+    else:
+        points, o = traj
 
-    print("Reading particle data...")
-    para, xp, v, mrat, beta, tags = particle_data(prefix, range(-7,10))
+    para, xp, v, mrat, beta, tags = particle_data(prefix, n)
 
     xp = xp[(tags != 0)]
     v = v[(tags != 0)]
     beta = beta[(tags != 0)]
     mrat = mrat[(tags != 0)]
 
-    print("Loading trajectory information...")
-    points, o = NH_tools.trajectory(NH_tools.close_start, NH_tools.close_end, 60.)
 
 
-    print("Generating spectrograms...")
     H, He, CH4 = spectrograms_by_species(xp, v, mrat, beta, points, o, radius=1187., progress=True)
 
     # The actual spectrogram to be plotted will be the total response of all species
@@ -32,6 +34,13 @@ def plot_espec(fig, ax, cbar_H, cbar_He, cbar_CH4, prefix, n):
     mH = np.ma.masked_array(tot_spec, mask=(~((H>He) & (H>CH4))))
     mHe = np.ma.masked_array(tot_spec, mask=(~((He>H) & (He>CH4))))
     mCH4 = np.ma.masked_array(tot_spec, mask=(~((CH4>H) & (CH4>He))))
+
+    return {'H':mH, 'He':mHe, 'CH4':mCH4, 'trajectory':points, 'orientation':o}
+
+def plot_espec(fig, ax, cbar_H, cbar_He, cbar_CH4, espec):
+
+    mH, mHe, mCH4 = espec['H'], espec['He'], espec['CH4']
+    points = espec['trajectory']
 
     # Plot the masked arrays
     Hhist = ax.pcolormesh(points[:,0]/1187., bin_centers, mH.T, norm=LogNorm(), cmap='Blues',
