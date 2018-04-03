040974e90ef162d4a579f4f19013c29f9774922b
Nathan Barnes <npbarnes@alaska.edu>
2018-04-03 10:59:23 -0800

    Setup for pressure profiles only

--- grid_profiles.py
+++ grid_profiles.py
@@ -15,9 +15,15 @@ assert unit('Boltzmann constant') == 'J K^-1'
 parser = argparse.ArgumentParser()
 parser.add_argument('hybrid_folder')
 parser.add_argument('--mccomas', action='store_true')
+parser.add_argument('-b', action='store_true')
 args = parser.parse_args()
 
 para = HybridParams(args.hybrid_folder).para
+
+def b_pressure(b):
+    b = 1.6726219e-27/1.60217662e-19 * b # proton gyrofrequency -> T
+    ret = np.linalg.norm(b, axis=-1)**2/(2*4*3.14159e-7)
+    return ret*1e12
 
 
 def pluto_position(p):
@@ -51,33 +57,42 @@ t_profile = profile(points, grid_points, tdata)
 pdata = k*ndata*tdata*(100**3)*(1e12) #pressure in pPa
 p_profile = profile(points, grid_points, pdata)
 
-fig, (vax, nax, tax, pax) = plt.subplots(4, sharex=True)
-vax.set_title("Simulation Flyby Profiles", fontsize=1.5*15)
+b = hr(args.hybrid_folder, 'bt').get_last_timestep()[-1]
+bdata = b_pressure(b)
+b_profile = profile(points, grid_points, bdata)
+
+#fig, (vax, nax, tax, pax) = plt.subplots(4, sharex=True)
+fig, pax = plt.subplots()
+#vax.set_title("Simulation Flyby Profiles", fontsize=1.5*15)
 
 pax.set_xlabel('X [$R_p$]')
 
 if args.mccomas:
     points[:,0] = -points[:,0]
 
-vax.plot(points[:,0]/1187., v_profile)
-nax.plot(points[:,0]/1187., n_profile)
-tax.plot(points[:,0]/1187., t_profile)
-pax.plot(points[:,0]/1187., p_profile)
+#vax.plot(points[:,0]/1187., v_profile)
+#nax.plot(points[:,0]/1187., n_profile)
+#tax.plot(points[:,0]/1187., t_profile)
+pax.plot(points[:,0]/1187., p_profile, label='Thermal Pressure')
+pax.plot(points[:,0]/1187., b_profile, label='Magnetic Pressure')
 
-vax.set_ylabel('<v$_{flow}$> [km/s]')
-nax.set_ylabel('Density [cm$^{-3}$]')
-tax.set_ylabel('Temp [K]')
+#vax.set_ylabel('<v$_{flow}$> [km/s]')
+#nax.set_ylabel('Density [cm$^{-3}$]')
+#tax.set_ylabel('Temp [K]')
 pax.set_ylabel('nkT [pPa]')
 
-nax.set_yscale('log')
-tax.set_yscale('log')
+#nax.set_yscale('log')
+#tax.set_yscale('log')
 pax.set_yscale('log')
 
+pax.set_title("Flyby pressure profile")
+pax.legend()
+
 if args.mccomas:
-    vax.set_xlim([10,100])
+    pax.set_xlim([10,100])
 else:
-    vax.invert_xaxis()
-    vax.set_xlim([-10,-100])
+    pax.invert_xaxis()
+    pax.set_xlim([-10,-100])
 
 plt.show()
 
