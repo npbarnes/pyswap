989c1637ebccfc27cb040ebb8639b983eff46758
Nathan Barnes <npbarnes@alaska.edu>
2018-04-03 11:02:08 -0800

    remove 'two' add medium and a function for onspecs

--- plot_especs.py
+++ plot_especs.py
@@ -2,24 +2,30 @@
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--high', action='store_true', 
-        help='Generate the high IMF spectrogram and not the low IMF spectrogram')
+        help='Generate the high IMF spectrogram and not the others')
+parser.add_argument('--medium', action='store_true', 
+        help='Generate the medium IMF spectrogram and not the others')
 parser.add_argument('--low', action='store_true', 
-        help='Generate the low IMF spectrogram and not the high IMF spectrogram')
-parser.add_argument('--two', action='store_true')
+        help='Generate the low IMF spectrogram and not the others')
 parser.add_argument('--other', action='store_true')
+parser.add_argument('--total-counts', action='store_true')
 
 args = parser.parse_args()
 
-if not args.high and not args.low and not args.two and not args.other:
+if not args.high and not args.medium and not args.low and not args.other:
     args.high = True
+    args.medium  = True
     args.low  = True
-    args.two  = True
 
 import cPickle
 from matplotlib import rcParams
 import matplotlib.pyplot as plt
-from espec import three_colorbars, plot_espec
+from espec import three_colorbars, plot_espec, plot_onespec
 from NH_tools import et_formatter
+
+def one_plot(filename, title, espec, fontsize=15):
+    fig, ax = plt.subplots()
+    plot_onespec(fig, ax, espec)
 
 def time_plot(filename, title, espec, fontsize=15):
     fig, ax = plt.subplots(figsize=(rcParams['figure.figsize'][0], 0.7*rcParams['figure.figsize'][1]))
@@ -52,17 +58,17 @@ if args.high:
         high_IMF_espec = cPickle.load(f)
     plot('high_IMF_synth_spec.png', "Synthetic SWAP Spectrogram\n0.3nT IMF", high_IMF_espec)
 
+if args.medium:
+    with open('medium_IMF_espec.pickle') as f:
+        medium_IMF_espec = cPickle.load(f)
+    plot('medium_IMF_synth_spec.png', "Synthetic SWAP Spectrogram\n0.19nT IMF", medium_IMF_espec)
+
 if args.low:
     with open('low_IMF_espec.pickle') as f:
         low_IMF_espec = cPickle.load(f)
     plot('low_IMF_synth_spec.png', "Synthetic SWAP Spectrogram\n0.08nT IMF", low_IMF_espec)
 
-if args.two:
-    with open('pluto-2-espec.pickle') as f:
-        low_IMF_espec = cPickle.load(f)
-    plot('synth_spec.png', "Synthetic SWAP Spectrogram\n0.08nT IMF\n 'Pluto-2'", low_IMF_espec)
-
 if args.other:
     with open('other_espec.pickle') as f:
         other_espec = cPickle.load(f)
-    time_plot('synth_spec.png', "Synthetic SWAP Spectrogram", other_espec)
+    one_plot('synth_spec.png', "Synthetic SWAP Spectrogram", other_espec)
