39b116f6a8421e620ff8755b23d6b3d04c738830
Nathan Barnes <npbarnes@alaska.edu>
2018-06-13 18:00:19 -0800

    Generalized grid_profiles

--- grid_profiles.py
+++ grid_profiles.py
@@ -4,102 +4,131 @@ import numpy as np
 from scipy.interpolate import RegularGridInterpolator
 from scipy.constants import value, unit
 from HybridReader2 import HybridReader2 as hr
+from HybridReader2 import NoSuchVariable
 from HybridParams import HybridParams
 import matplotlib.pyplot as plt
 import spice_tools
-import argparse
 
-eV2J = value('Boltzmann constant in eV/K')
-k = value('Boltzmann constant')
-assert unit('Boltzmann constant') == 'J K^-1'
+# Profile interpolator
+def profile(interp_points, grid_points, grid_data):
+    rgi = RegularGridInterpolator(points=grid_points, values=grid_data, bounds_error=False)
+    return rgi(interp_points)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--mccomas', action='store_true')
-parser.add_argument('--hybrid-folder', dest='hybrid_folder', default=None)
-parser.add_argument('-b', action='store_true')
-parser.add_argument('--prefix', default='data')
-args = parser.parse_args()
+# Get trajectory points
+points, _, _ = spice_tools.trajectory(spice_tools.flyby_start, spice_tools.flyby_end, 60.)
 
-if args.hybrid_folder is None:
-    args.hybrid_folder = os.path.join(os.getcwd(), args.prefix)
-else:
-    args.hybrid_folder = os.path.join(args.hybrid_folder, args.prefix)
 
-para = HybridParams(args.hybrid_folder).para
+# Define functions for getting the values we want
+def vmag(v):
+    return np.linalg.norm(v, axis=-1)
+
+def n_km(n):
+    return n*1e-15
+
+def t_K(t):
+    eV_per_K = value('Boltzmann constant in eV/K')
+    return t/eV_per_K
+
+def pressure_pPa(n,t):
+    k = value('Boltzmann constant')
+    assert unit('Boltzmann constant') == 'J K^-1'
+    return k*n*t*(100**3)*(1e12)
 
 def b_pressure(b):
     b = 1.6726219e-27/1.60217662e-19 * b # proton gyrofrequency -> T
     ret = np.linalg.norm(b, axis=-1)**2/(2*4*3.14159e-7)
     return ret*1e12
 
+def density_profile(hf, step=-1):
+    hybrid_n = hr(hf, 'np')
+    grid_points = hybrid_n.para['grid_points']
+    n = hybrid_n.get_timestep(step)[-1]
+    ndata = n_km(n)
+    return profile(points, grid_points, ndata)
 
-def pluto_position(p):
-    """get the position of pluto in simulation coordinates"""
-    return p['qx'][p['nx']/2 + p['pluto_offset']]
+def pressure_profile(hf, step=-1):
+    hybrid_n = hr(hf, 'np')
+    grid_points = hybrid_n.para['grid_points']
+    n = hybrid_n.get_timestep(step)[-1]
+    ndata = n_km(n)
 
-qx = para['qx'] - pluto_position(para)
-qy = para['qy'] - para['qy'][-1]/2
-qz = para['qzrange'] - para['qzrange'][-1]/2
+    try:
+        hybrid_temp = hr(hf, 'temp_tot')
+    except NoSuchVariable:
+        hybrid_temp = hr(hf, 'temp_p')
 
-grid_points = qx, qy, qz
+    t = hybrid_temp.get_timestep(step)[-1]
+    tdata = t_K(t)
 
-def profile(interp_points, grid_points, grid_data):
-    rgi = RegularGridInterpolator(points=grid_points, values=grid_data, bounds_error=False)
-    return rgi(interp_points)
+    thermal_p_data = pressure_pPa(ndata, tdata)
 
-points, o, times = spice_tools.trajectory(spice_tools.flyby_start, spice_tools.flyby_end, 60.)
+    hybrid_b = hr(hf, 'bt')
+    b = hybrid_b.get_timestep(step)[-1]
+    bdata = b_pressure(b)
 
-v = hr(args.hybrid_folder,'up').get_last_timestep()[-1]
-vdata = np.linalg.norm(v, axis=-1)
-v_profile = profile(points, grid_points, vdata)
+    p_therm = profile(points, grid_points, thermal_p_data)
+    p_b     = profile(points, grid_points, bdata)
 
-n = hr(args.hybrid_folder, 'np').get_last_timestep()[-1]
-ndata = n*1e-15
-n_profile = profile(points, grid_points, ndata)
+    return zip(p_therm, p_b)
 
-t = hr(args.hybrid_folder,'temp_p').get_last_timestep()[-1]
-tdata = t/eV2J
-t_profile = profile(points, grid_points, tdata)
 
-pdata = k*ndata*tdata*(100**3)*(1e12) #pressure in pPa
-p_profile = profile(points, grid_points, pdata)
+def main(profile_func, title='Flyby Profile', labels=[], xlabel='', ylabel=''):
+    import argparse
 
-b = hr(args.hybrid_folder, 'bt').get_last_timestep()[-1]
-bdata = b_pressure(b)
-b_profile = profile(points, grid_points, bdata)
+    # Some usage stuff
+    def limitType(s):
+        if s == 'auto':
+            return None
+        else:
+            return float(s)
 
-#fig, (vax, nax, tax, pax) = plt.subplots(4, sharex=True)
-fig, pax = plt.subplots()
-#vax.set_title("Simulation Flyby Profiles", fontsize=1.5*15)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mccomas', action='store_true')
+    parser.add_argument('--hybrid-folder', dest='hybrid_folder', default=None)
+    parser.add_argument('--prefix', default='data')
+    parser.add_argument('--step', type=int, default=-1)
+    parser.add_argument('--xlim', type=limitType, nargs=2, default=[10,100])
+    parser.add_argument('--ylim', type=limitType, nargs=2, default=[0.008,1.1])
 
-pax.set_xlabel('X [$R_p$]')
+    args = parser.parse_args()
 
-if args.mccomas:
-    points[:,0] = -points[:,0]
+    if args.hybrid_folder is None:
+        args.hybrid_folder = os.path.join(os.getcwd(), args.prefix)
+    else:
+        args.hybrid_folder = os.path.join(args.hybrid_folder, args.prefix)
 
-#vax.plot(points[:,0]/1187., v_profile)
-#nax.plot(points[:,0]/1187., n_profile)
-#tax.plot(points[:,0]/1187., t_profile)
-pax.plot(points[:,0]/1187., p_profile, label='Thermal Pressure')
-pax.plot(points[:,0]/1187., b_profile, label='Magnetic Pressure')
+    my_profile = profile_func(args.hybrid_folder, args.step)
 
-#vax.set_ylabel('<v$_{flow}$> [km/s]')
-#nax.set_ylabel('Density [cm$^{-3}$]')
-#tax.set_ylabel('Temp [K]')
-pax.set_ylabel('nkT [pPa]')
+    # Make a figure with an axis
+    fig, ax = plt.subplots()
 
-#nax.set_yscale('log')
-#tax.set_yscale('log')
-pax.set_yscale('log')
+    if args.mccomas:
+        points[:,0] = -points[:,0]
 
-pax.set_title("Flyby pressure profile")
-pax.legend()
+    lines = ax.plot(points[:,0]/1187., my_profile)
+    for line, label in zip(lines, labels):
+        line.set_label(label)
 
-if args.mccomas:
-    pax.set_xlim([10,100])
-else:
-    pax.invert_xaxis()
-    pax.set_xlim([-10,-100])
+    ax.legend(loc='best')
+    ax.set_yscale('log')
 
-plt.show()
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
 
+    if args.mccomas:
+        ax.set_xlim(*args.xlim)
+    else:
+        ax.invert_xaxis()
+        ax.set_xlim(*[-x for x in args.xlim])
+
+    ax.set_ylim(*args.ylim)
+
+
+    plt.savefig(os.path.join(args.hybrid_folder,'../pressure.png'))
+
+
+if __name__ == '__main__':
+    #main(density_profile)
+    plt.style.use('pluto-paper')
+    main(pressure_profile, title='Pressure Profiles\n0.19nT', labels=('Thermal Pressure','Magnetic Pressure'), xlabel='X ($R_p$)', ylabel='Pressure (pPa)')
