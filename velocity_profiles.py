f9c5b932ea409e0ec37d0addcf804e2ac95b3e92
Nathan Barnes <npbarnes@alaska.edu>
2018-06-27 16:56:46 -0800

    Update for multiple profiles. Could still be merged with grid_profiles.

--- velocity_profiles.py
+++ velocity_profiles.py
@@ -14,16 +14,19 @@ k = value('Boltzmann constant')
 assert unit('Boltzmann constant') == 'J K^-1'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('hybrid_folder')
+parser.add_argument('--save', action='store_true')
 parser.add_argument('--mccomas', action='store_true')
 args = parser.parse_args()
 
-args.high = join(args.hybrid_folder,'pluto-7','data')
-args.low  = join(args.hybrid_folder,'pluto-8','data')
+prefixes = ['/home/nathan/data/2017-Mon-Nov-13/pluto-7/data',
+            '/home/nathan/data/2018-Fri-Jan-26/pluto-1/data',
+            '/home/nathan/data/2017-Mon-Nov-13/pluto-8/data',
+            '/home/nathan/data/2018-Wed-Jun-06/pluto-1/data']
+labels = ['0.3nT', '0.19nT', '0.08nT', 'No Shell 0.19nT']
 
-high_para = HybridParams(args.high).para
-low_para = HybridParams(args.low).para
-
+paras = []
+for p in prefixes:
+    paras.append(HybridParams(p).para)
 
 def pluto_position(p):
     """get the position of pluto in simulation coordinates"""
@@ -37,8 +40,9 @@ def get_grid_points(para):
     grid_points = qx, qy, qz
     return grid_points
 
-high_grid = get_grid_points(high_para)
-low_grid = get_grid_points(low_para)
+grids = []
+for p in paras:
+    grids.append(get_grid_points(p))
 
 def profile(interp_points, grid_points, grid_data):
     rgi = RegularGridInterpolator(points=grid_points, values=grid_data, bounds_error=False)
@@ -46,28 +50,28 @@ def profile(interp_points, grid_points, grid_data):
 
 points, o = NH_tools.trajectory(NH_tools.close_start, NH_tools.close_end, 60.)
 
-v = hr(args.high,'up').get_last_timestep()[-1]
-vdata = np.linalg.norm(v, axis=-1)
-high_profile = profile(points, high_grid, vdata)
+profiles = []
+for pre,grid in zip(prefixes,grids):
+    v = hr(pre,'up').get_last_timestep()[-1]
+    vdata = np.linalg.norm(v,axis=-1)
+    profiles.append(profile(points, grid, vdata))
 
-v = hr(args.low,'up').get_last_timestep()[-1]
-vdata = np.linalg.norm(v, axis=-1)
-low_profile = profile(points, low_grid, vdata)
-
+plt.style.use('pluto-paper')
 fig, vax = plt.subplots()
-vax.set_title("Simulation Flyby Velocity Profile", fontsize=1.5*15)
+vax.set_title("Simulation Flyby Velocity Profiles")
 
-vax.set_xlabel('X [$R_p$]')
+vax.set_xlabel('X ($R_p$)')
 
 if args.mccomas:
     points[:,0] = -points[:,0]
 
-vax.plot(points[:,0]/1187., high_profile, label='0.3nT')
-vax.plot(points[:,0]/1187., low_profile, label='0.08nT')
-vax.plot([-12.7,3.8,8.8,13.1,18.9,158.7,175.2],[400.,365.,324.,250.,140.,320.,400.], marker='o', linestyle='None', label='Data', color='C2')
+for p,l in zip(profiles, labels):
+    vax.plot(points[:,0]/1187., p, label=l)
+
+vax.plot([-12.7,3.8,8.8,13.1,18.9,158.7,175.2],[400.,365.,324.,250.,140.,320.,400.], marker='o', linestyle='None', label='Data')
 vax.legend()
 
-vax.set_ylabel('<v$_{flow}$> [km/s]')
+vax.set_ylabel('|v| (km/s)')
 
 if args.mccomas:
     vax.set_xlim([-20,200])
@@ -75,5 +79,8 @@ else:
     vax.invert_xaxis()
     vax.set_xlim([-10,-100])
 
-plt.show()
+if args.save:
+    plt.savefig('sw_slowing_mccomas_comparison', bbox_inches='tight')
+else:
+    plt.show()
 
